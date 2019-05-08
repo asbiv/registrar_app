fda4a6cfcaedc925f8dec95386fefe80f00a38ca
errfrank <eric.franklin48@gmail.com>
2019-05-08 13:15:33 -0400

    js updates excel

--- js/form-data-to-json.js
+++ js/form-data-to-json.js
@@ -9,7 +9,7 @@ function sendToAlg(form){
     ABjsonURL = 'https://registrar-api.herokuapp.com/?query'+'='+JSON.stringify(jsonData);
  
     $.getJSON(ABjsonURL, function (data) {
-        var tableStart = "<table style='width:100%;overflow-x:auto;'><tr><th>Course Number</th><th>Title</th><th>Quarter</th><th>Week</th><th>Time</th></tr>"
+        var tableStart = "<button class='prevNext' onclick='fnExcelReport();'> EXPORT TO EXCEL </button><table id='result_course_table' style='width:100%;overflow-x:auto;'><tr><th>Course Number</th><th>Title</th><th>Quarter</th><th>Week</th><th>Time</th></tr>"
         var i;
         for (i = 0; i < data.length; i++) { 
         if (data[i].RegisterClassBinary==1) {
@@ -23,6 +23,39 @@ function sendToAlg(form){
 }) 
 }
 
+function fnExcelReport()
+{
+    var tab_text="<table border='2px'><tr bgcolor='#87AFC6'>";
+    var textRange; var j=0;
+    tab = document.getElementById('result_course_table'); // id of table
+
+    for(j = 0 ; j < tab.rows.length ; j++) 
+    {     
+        tab_text=tab_text+tab.rows[j].innerHTML+"</tr>";
+        //tab_text=tab_text+"</tr>";
+    }
+
+    tab_text=tab_text+"</table>";
+    tab_text= tab_text.replace(/<A[^>]*>|<\/A>/g, "");//remove if u want links in your table
+    tab_text= tab_text.replace(/<img[^>]*>/gi,""); // remove if u want images in your table
+    tab_text= tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE "); 
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer
+    {
+        txtArea1.document.open("txt/html","replace");
+        txtArea1.document.write(tab_text);
+        txtArea1.document.close();
+        txtArea1.focus(); 
+        sa=txtArea1.document.execCommand("SaveAs",true,"Say Thanks to Sumit.xls");
+    }  
+    else                 //other browser not tested on IE 11
+        sa = window.open('data:application/vnd.ms-excel,' + encodeURIComponent(tab_text));  
+
+    return (sa);
+}
 
 //add headers, add if statement to pull only 1 binary
 
